a1739d428d76bfe303fdd9994af884b8addeaf77
Imrane <98223476+iimraane@users.noreply.github.com>
2024-04-15 11:03:41 +0100

    Adapted for public

--- script.py
+++ script.py
@@ -23,15 +23,15 @@ username_input = driver.find_element(By.NAME, "username")
 password_input = driver.find_element(By.NAME, "password")
 login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
 
-username_input.send_keys("El_titano___")
-password_input.send_keys("Imrane789")
+username_input.send_keys("YOUR EMAIL HERE")
+password_input.send_keys("YOUR PASSWORD HERE")
 login_button.click()
 
 # Attendre que la page soit entièrement chargée après la connexion
 time.sleep(3)
 
 # URL du post vers lequel vous souhaitez naviguer
-url_du_post = "https://www.instagram.com/p/C5k8ipbrcjN/?img_index=1"
+url_du_post = "URL OF YOUR POST HERE"
 driver.get(url_du_post)
 
 # Exécuter le script JavaScript pour faire défiler la page
@@ -44,7 +44,7 @@ text_zone = driver.find_element(By.CSS_SELECTOR, "textarea.x1i0vuye")
 mot =[]
 
 while True:
-# Générer un mot aléatoire
+    # Générer un mot aléatoire
     letters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '_', '`', '{', '|', '}', '~', 'a', 'b', 'c', 'd', 'e', 'f,' 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
     mot = ''.join(random.choices(letters, k=12))
 
